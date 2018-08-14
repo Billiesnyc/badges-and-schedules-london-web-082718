438c54b86c893d0d3f009a1120935e7f8113393f
Billie Simmons <billiesnyc@gmail.com>
2018-08-14 13:11:37 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,7 +19,7 @@ def assign_rooms(name_list)
   assigned_rooms_list
 end
 
-def printer(name_list)
+def printer(attendees)
   batch_badge_creator.all.each do |statement|
     puts statement.inspect
   end
