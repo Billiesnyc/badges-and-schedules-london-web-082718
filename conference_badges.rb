fecce28da029469a136f21cae979133f8f2bc0a2
Billie Simmons <billiesnyc@gmail.com>
2018-08-14 11:51:29 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,5 @@
 # Write your code here.
 def batch_badge_creator(name_list)
   name_list.each do |name|
-    puts "Hello, #{name}! You'll be assigned to room " + name[i].to_i
+    puts "Hello, #{name}! You'll be assigned to room " + name[i].to_i + 1 + "!"
+  end
